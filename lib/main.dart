34419eb0915f2346b6bbb5d651471d9632139e1c
Yurii <131777946+Xenus4737379@users.noreply.github.com>
2024-10-11 08:35:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,84 +10,272 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ігрові новини',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: LoginPage(),
+      routes: {
+        '/Логін': (context) => LoginPage(),
+        '/Регістрація': (context) => RegisterPage(),
+        '/Профіль': (context) => ProfilePage(
+              username: savedUsername,
+              email: savedEmail,
+              password: savedPassword,
+            ),
+        '/Головна сторінка': (context) => const HomePage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+// Створимо змінні для збереження даних користувача
+String savedUsername = '';
+String savedEmail = '';
+String savedPassword = '';
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+// Екран Логіну
+class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  final TextEditingController _controller = TextEditingController();
-  String? _errorMessage;  // Для виведення повідомлення про помилку
-
-  @override
-  void dispose() {
-    _controller.dispose();  // Звільняємо ресурс, коли віджет видаляється
-    super.dispose();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      final String input = _controller.text;
-
-      if (int.tryParse(input) != null) {
-        _counter += int.parse(input);
-        _errorMessage = null;  // Очищаємо повідомлення про помилку
-      } else if (input == 'Avada Kedavra') {
-        _counter = 0;
-        _errorMessage = null;
-      } else {
-        _errorMessage = 'Введіть число або "Avada Kedavra"';
-      }
-
-      _controller.clear();
-    });
-  }
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Інтерактивне поле введення'),
-      ),
-      body: Center(
+      appBar: AppBar(title: const Text('Вхід')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Інкремент: $_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(labelText: "Ім'я"),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: 'Введіть число або "Avada Kedavra"',
-                  errorText: _errorMessage,  // Виводимо повідомлення про помилку
-                ),
-                keyboardType: TextInputType.text,
-              ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Пароль'),
+              obscureText: true,
             ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Оновити інкремент'),
+            const SizedBox(height: 20),
+            CustomButton(
+              text: 'Вхід',
+              onPressed: () {
+                if (usernameController.text == savedUsername &&
+                    passwordController.text == savedPassword) {
+                  Navigator.pushNamed(context, '/Головна сторінка');
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content:
+                          Text("Неправильне ім'я чи пароль, спробуйте ще раз"),
+                    ),
+                  );
+                }
+              },
+            ),
+            CustomButton(
+              text: 'Регістрація',
+              onPressed: () {
+                Navigator.pushNamed(context, '/Регістрація');
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+// Екран Реєстрації
+class RegisterPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Регістрація')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(labelText: "Ім'я"),
+            ),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Емейл'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Пароль'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              text: 'Регістрація',
+              onPressed: () {
+                savedUsername = usernameController.text;
+                savedEmail = emailController.text;
+                savedPassword = passwordController.text;
+
+                Navigator.pushNamed(context, '/Логін');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Екран Профілю
+class ProfilePage extends StatelessWidget {
+  final String username;
+  final String email;
+  final String password;
+
+  const ProfilePage({
+    required this.username,
+    required this.email,
+    required this.password,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Профіль')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              'З ПОВЕРНЕННЯМ, $username!',
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Логін: $username',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Емейл: $email',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Пароль: $password',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Головна Сторінка
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ігрові новини')),
+      body: SingleChildScrollView(
+        // Додаємо прокручуваний віджет
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Розділ 1
+            const Text(
+              'Довгоочікувана Red Dead Redemption нарешті буде на компютерах.',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Культова гра Red Dead Redemption та її доповнення у стилі зомбі-горрор, Undead Nightmare, будуть доступні на ПК з 29 жовтня 2024 року.\n'
+              'Про це Rockstar повідомила у своєму пресрелізі. Версія для ПК отримає кілька покращень, розроблених для використання переваг сучасного обладнання.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+
+            // Розділ 2
+            const Text(
+              'Ремейк Silent Hill 2 "злили" в інтернет – за кілька днів до офіційного релізу.',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Компанія Konami відкрила ранній доступ до довгоочікуваного ремейка Silent Hill 2 для всіх, хто попередньо замовив розширене видання гри на ПК і PS5.\n'
+              'Виявилося, що у Silent Hill 2 Remake не було ніякого захисту від злому, тому делюкс-версія, що дає ранній доступ до гри, "витекла" на торенти прямо в день виходу. І за два дні до повноцінного релізу для всіх, хто вибрав стандартне видання.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+
+            // Розділ 3
+            const Text(
+              'Ремастер Until Dawn стартував гірше, ніж скандальний Concord',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Ремастер Until Dawn стартував на ринку з труднощами та викликав змішану реакцію. Статистика показує, що запуск гри від студії Ballistic Moon значно поступається іншим релізам, включно з багатостраждальним шутером Concord.\n'
+              'Старт гри на 28,6% слабший, ніж у Concord, і однією з головних причин гравці називають занадто високу ціну.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+
+            // Додаємо кнопку для переходу до профілю
+            Center(
+              child: CustomButton(
+                text: 'До профілю',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/Профіль');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Перевикористовуваний віджет Кнопки
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({required this.text, required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
     );
   }
 }
